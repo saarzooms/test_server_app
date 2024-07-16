3643e164b2387453166b61d2e03479170e667dbb
VATALIYABHAVIL <vataliyabhavil3@gmail.com>
2024-07-16 18:02:32 +0530

    updating display name,Airthmatic equation.

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const app = express()
     //end point - root(/)
-app.get('/', (req, res) => {
-    res.send('Hello There !!!!')
+app.get('/name/:n', (req, res) => {
+    res.send('Welcom!' + req.params.n)
+}),
+//display name
+app.get('/name/:fn/:ln', (req, res) => {
+    res.send('Welcom!' +req.params.fn+'' +req.params.ln)
+}),
+//Airthmatic eqaution
+app.get('/Add/:fn/:sn', (req, res) => {
+    res.send('Add' + (parseInt(req.params.fn)+ parseInt(req.params.sn)))
+})
+app.get('/Sub/:fn/:sn', (req, res) => {
+    res.send('Sub' + (parseInt(req.params.fn)- parseInt(req.params.sn)))
+})
+app.get('/Multi/:fn/:sn', (req, res) => {
+    res.send('Multi' + (parseInt(req.params.fn)* parseInt(req.params.sn)))
+})
+app.get('/Div/:fn/:sn', (req, res) => {
+    res.send('Div' + (parseInt(req.params.fn)/ parseInt(req.params.sn)))
+})
+app.delete('/name', (req, res) => {
+    res.send('Keval')
 })
 
 app.listen(4000, () => {
